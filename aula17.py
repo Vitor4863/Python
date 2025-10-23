314b9169686cad0d3ba5b60c51f91c8780a0ad60
unknown <joaovitosborges@outlook.com>
2025-10-23 00:33:45 -0300

    atualizar

---
+++ aula17.py
@@ -0,0 +1,10 @@
+total = 0 
+
+for i in range(0,5):
+    num = int(input("Digite um numero: "))
+    total += num
+
+
+print('Soma: %s' % (total))
+print('Media %s' % (total / 5))
+    
---
+++ aula18.py
@@ -0,0 +1,4 @@
+tab = int(input('Tabuada do numero:'))
+
+for i in range(10):
+    print('%d x %d = %d' % (tab, i + 1 , tab*(i+1)))
